4783bafef11fa5736d14e9ecc90eba408867999f
Paul Meaney <p.meaney3@nuigalway.ie>
2018-08-06 23:39:49 +0100

    complete alogrithm with results

--- classes.py
+++ classes.py
@@ -3,12 +3,13 @@ import enchant #this library contains a dictionary
 class Word:
     dict = enchant.Dict("en_US")
 
-    def __init__(self, inputWord):
+    def __init__(self, inputWord, order = []):
         self.currentWord = inputWord
+        self.wordLength = len(inputWord)
+        self.index_order = order
 
     def check_if_valid_word(self, word):
         return self.dict.check(word)
-
 
     def add_letter(self, letter):
         str_list = list(self.currentWord)
@@ -16,8 +17,6 @@ class Word:
         for i in range(0, len(str_list)+1):
             str_list.insert(i, letter)
             if self.check_if_valid_word(''.join(str_list)):
-                valid_words.append(Word(''.join(str_list)))
-
+                valid_words.append(Word(''.join(str_list), (self.index_order + [i])))
             del str_list[i]
-
         return valid_words
--- solver.py
+++ solver.py
@@ -1,10 +1,41 @@
 from classes import Word
+import string
 
-sample = Word("coast")
+def find_words(list_of_words):
+    letters = list(string.ascii_lowercase)
+    words = []
+    for word in list_of_words:
+        for l in letters:
+            new = word.add_letter(l)
+            if len(new) > 0:
+                words = words + new
+            new = []
 
-new_words = sample.add_letter('s')
+    return words
 
-if (len(new_words) == 0):
-    print "no words"
-for word in new_words:
-    print word.currentWord
+def largest_word_len(list_of_words):
+    count = 0
+    for word in list_of_words:
+        if word.wordLength > count:
+            count = word.wordLength
+    return count
+
+masterlist = [Word("a")]
+count = 0
+
+while count < 8:
+    masterlist = find_words(masterlist)
+    count = largest_word_len(masterlist)
+
+for w in masterlist:
+    print w.currentWord
+    print w.index_order
+
+f = open("results.txt", 'w')
+for w in masterlist:
+    f.write(w.currentWord)
+    f.write("\n")
+    for index in w.index_order:
+        f.write("%d" % index)
+        f.write(",")
+    f.write("\n")
